238e3a6fb554d2bfa4496fced3bc09b5c5c38df7
ByeongUkJeon <jbo6659@gmail.com>
2023-12-28 02:57:11 +0900

    test 커밋

--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+import json
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views import View
 # Create your views here.
 
-def index(request):
-    return render(request,"pybo/index.html")
+def post(request, *args, **kwargs):
+    try:
+        data = json.loads(request.body)
+        print(data)
+        product_id = data["id"]
+        skintype = data["skintype"]
 
+        return render(request, "pybo/index.html", data)
+    except KeyError:
+        return JsonResponse({"message": KEY_ERROR}, status=400)
+
+def index(request):
+
+
+    return render(request,"pybo/index.html")
